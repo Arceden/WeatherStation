4954156ca7449a478ac62dbdf01182864870749f
Arnold217 <arnoldbuining@hotmail.com>
2019-01-22 20:10:55 +0100

    Added a babel minifier to the gulpfile

--- Website/gulpfile.js
+++ Website/gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minify = require('gulp-babel-minify');
 
 gulp.task('sass', function(done){
 	gulp.src('./dev/sass/**/*.scss')
@@ -8,6 +9,16 @@ gulp.task('sass', function(done){
 		.pipe(gulp.dest('./build/css'));
 		done();
 });
+
+gulp.task("minify", () =>
+	gulp.src("./build/js/*.js")
+		.pipe(minify({
+			mangle: {
+				keepClassName: true
+			}
+		}))
+		.pipe(gulp.dest("./build/js"))
+);
 
 gulp.task('sass:watch', function(){
 	gulp.watch('./dev/sass/**/*.scss', gulp.series('sass'));
