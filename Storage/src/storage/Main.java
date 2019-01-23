24ddea055cde59a0b499dbb3fe1453469d5e5615
Arnold217 <arnoldbuining@hotmail.com>
2019-01-23 20:53:04 +0100

    Added a simple querry

---
+++ Storage/src/storage/Main.java
@@ -0,0 +1,12 @@
+package storage;
+
+public class Main {
+
+    public static void main(String args[]){
+
+        StorageManager sm = new StorageManager();
+        sm.readBuffer(sm.loadData());
+
+    }
+
+}
