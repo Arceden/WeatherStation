ce580d8026ce2f3b0d7c4b5067bfe537aeb87fae
matteogeertsema <m.geertsema@st.hanze.nl>
2019-02-04 13:21:38 +0100

    connectie met database toegevoegd

---
+++ Website/build/Asseater69/config.php
@@ -0,0 +1,17 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$dbServer = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "mydb";
+ 
+/* Attempt to connect to MySQL database */
+try{
+    $pdo = new PDO("mysql:host=$dbServer;dbname=$dbName", $dbUsername, $dbPassword);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+?>
